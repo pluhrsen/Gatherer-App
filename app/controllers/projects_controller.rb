17234c07144c4908443fc3075b045174178e1471
paulluhrsen <paulluhrsen@gmail.com>
2020-05-15 14:07:05 -0500

    first system test

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,16 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def index
+    @projects = Project.all
+  end
+
+  def create
+    @workflow = CreatesProject.new(
+        name: params[:project][:name],
+        task_string: params[:project][:tasks])
+    @workflow.create
+    redirect_to projects_path
+  end
 end
